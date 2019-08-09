94a7e2b7c21bf314dbb69e9e157077926589bae3
sdaghigh <ucontacti2012@gmail.com>
2019-08-09 17:37:26 +0200

    Complete Random Forest Algorithm added

--- binaryClassifier.py
+++ binaryClassifier.py
@@ -1,17 +1,145 @@
-import numpy as np
-import scipy as sp
-import matplotlib.pyplot as plt
+# In[1]: Header and load data
+
+
+import numpy as np 
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+train_df=pd.read_csv("data/train.csv")
+train_df.head()
 
 
+# In[2]: Find and show missing data
+
+
+def missingdata(data):
+    total = data.isnull().sum().sort_values(ascending = False)
+    percent = (data.isnull().sum()/data.isnull().count()*100).sort_values(ascending = False)
+    ms=pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
+    ms= ms[ms["Percent"] > 0]
+    f,ax =plt.subplots(figsize=(8,6))
+    plt.xticks(rotation='90')
+    fig=sns.barplot(ms.index, ms["Percent"],color="green",alpha=0.8)
+    plt.xlabel('Features', fontsize=15)
+    plt.ylabel('Percent of missing values', fontsize=15)
+    plt.title('Percent missing data by feature', fontsize=15)
+    return ms
+missingdata(train_df)
+
+
+# In[3]: Data Cleaning
+
+
+train_df['Embarked'].fillna(train_df['Embarked'].mode()[0], inplace = True)
+train_df['Age'].fillna(train_df['Age'].median(), inplace = True)
+drop_column = ['Cabin']
+train_df.drop(drop_column, axis=1, inplace = True)
+print('check the nan value in train data')
+print(train_df.isnull().sum())
+
+
+
+# In[7]: Feauture Engineering
+
+
+dataset = train_df
+dataset['FamilySize'] = dataset['SibSp'] + dataset['Parch'] + 1
+
+import re
+# Define function to extract titles from passenger names
+def get_title(name):
+    title_search = re.search(' ([A-Za-z]+)\.', name)
+    # If the title exists, extract and return it.
+    if title_search:
+        return title_search.group(1)
+    return ""
+# Create a new feature Title, containing the titles of passenger names
+dataset['Title'] = dataset['Name'].apply(get_title)
+# Group all non-common titles into one single grouping "Rare"
+dataset['Title'] = dataset['Title'].replace(['Lady', 'Countess','Capt', 'Col','Don','Dr', 'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Rare')
+dataset['Title'] = dataset['Title'].replace('Mlle', 'Miss')
+dataset['Title'] = dataset['Title'].replace('Ms', 'Miss')
+dataset['Title'] = dataset['Title'].replace('Mme', 'Mrs')
+dataset['Age_bin'] = pd.cut(dataset['Age'], bins=[0,14,20,40,120], labels=['Children','Teenage','Adult','Elder'])
+dataset['Fare_bin'] = pd.cut(dataset['Fare'], bins=[0,7.91,14.45,31,120], labels=['Low_fare','median_fare', 'Average_fare','high_fare'])
+                                                                               
+traindf=train_df
+drop_column = ['Age','Fare','Name','Ticket']
+train_df.drop(drop_column, axis=1, inplace = True)
+drop_column = ['PassengerId']
+traindf.drop(drop_column, axis=1, inplace = True)
+traindf = pd.get_dummies(traindf, columns = ["Sex","Title","Age_bin","Embarked","Fare_bin"],
+                             prefix=["Sex","Title","Age_type","Em_type","Fare_type"])
+
+sns.heatmap(traindf.corr(),annot=True,cmap='RdYlGn',linewidths=0.2) #data.corr()-->correlation matrix
+fig=plt.gcf()
+fig.set_size_inches(20,12)
+plt.show()
+
+
+# In[14]: Making train and test data ready
+
+from sklearn.model_selection import train_test_split #for split the data
+from sklearn.metrics import accuracy_score  #for accuracy_score
+from sklearn.model_selection import KFold #for K-fold cross validation
+from sklearn.model_selection import cross_val_score #score evaluation
+from sklearn.model_selection import cross_val_predict #prediction
+from sklearn.metrics import confusion_matrix #for confusion matrix
+all_features = traindf.drop("Survived",axis=1)
+Targeted_feature = traindf["Survived"]
+X_train,X_test,y_train,y_test = train_test_split(all_features,Targeted_feature,test_size=0.3,random_state=42)
+X_train.shape,X_test.shape,y_train.shape,y_test.shape
+
+
+# In[15]: Random Forest Learning
+
+
+from sklearn.ensemble import RandomForestClassifier
+model = RandomForestClassifier(criterion='gini', n_estimators=700,
+                             min_samples_split=10,min_samples_leaf=1,
+                             max_features='auto',oob_score=True,
+                             random_state=1,n_jobs=-1)
+model.fit(X_train,y_train)
+prediction_rm=model.predict(X_test)
+print('--------------The Accuracy of the model----------------------------')
+print('The accuracy of the Random Forest Classifier is', round(accuracy_score(prediction_rm,y_test)*100,2))
+kfold = KFold(n_splits=10, random_state=22) # k=10, split the data into 10 equal parts
+result_rm=cross_val_score(model,all_features,Targeted_feature,cv=10,scoring='accuracy')
+print('The cross validated score for Random Forest Classifier is:',round(result_rm.mean()*100,2))
+y_pred = cross_val_predict(model,all_features,Targeted_feature,cv=10)
+sns.heatmap(confusion_matrix(Targeted_feature,y_pred),annot=True,fmt='3.0f',cmap="summer")
+plt.title('Confusion_matrix', y=1.05, size=15)
+
+
+# In[16]: Tuning
+
+
+model = RandomForestClassifier(criterion='gini', n_estimators=700,
+                             min_samples_split=10,min_samples_leaf=1,
+                             max_features='auto',oob_score=True,
+                             random_state=1,n_jobs=-1)
+
+
+from sklearn.model_selection import GridSearchCV# Random Forest Classifier Parameters tunning 
+model = RandomForestClassifier()
+n_estim=range(100,1000,100)## Search grid for optimal parameters
+param_grid = {"n_estimators" :n_estim}
+model_rf = GridSearchCV(model,param_grid = param_grid, cv=5, scoring="accuracy", n_jobs= 4, verbose = 1)
+model_rf.fit(X_train,y_train)# Best score
+print(model_rf.best_score_)#best estimator
+model_rf.best_estimator_
+
+
+# In[20]:
 def plotData2D(X, filename=None):
     fig = plt.figure()
     axs = fig.add_subplot(111)
 
-    axs.plot(X[0,:], X[1,:], 'ro', label = 'data')
+    axs.plot(X[0, :], X[1, :], 'ro', label='data')
     if filename == None:
         plt.show()
     plt.close()
+
 
 def plotClusters(X, k, clusters, filename=None):
     # create a figure and its axes
@@ -20,8 +148,8 @@ def plotClusters(X, k, clusters, filename=None):
 
     # plot the data
     colors = ["red", "green", "blue", "yellow"]
-    for i in range(np.size(X,1)):
-        plt.scatter(X[0][i], X[1][i],color = colors[clusters[i]])
+    for i in range(np.size(X, 1)):
+        plt.scatter(X[0][i], X[1][i], color=colors[clusters[i]])
 
     # either show figure on screen or write it to disk
     if filename == None:
@@ -30,37 +158,22 @@ def plotClusters(X, k, clusters, filename=None):
         plt.savefig(filename)
     plt.close()
 
-def linearClassifier(X, y): #TODO: lift data
+def missingdata(data):
+    total = data.isnull().sum().sort_values(ascending=False)
+    percent = (data.isnull().sum()/data.isnull().count()
+                * 100).sort_values(ascending=False)
+    ms = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
+    ms = ms[ms["Percent"] > 0]
+    f, ax = plt.subplots(figsize=(8, 6))
+    plt.xticks(rotation='90')
+    fig = sns.barplot(ms.index, ms["Percent"], color="green", alpha=0.8)
+    plt.xlabel('Features', fontsize=15)
+    plt.ylabel('Percent of missing values', fontsize=15)
+    plt.title('Percent missing data by feature', fontsize=15)
+    return ms
+
+
+def linearClassifier(X, y):  # TODO: lift data
     W = np.linalg.inv(np.dot(X, X.T))
     W = np.dot(np.dot(W, X), y)
-    return np.dot(X.T, W), np.sum(np.power(np.dot(X.T, W) - y, 2))/np.size(X,1)
-
-if __name__ == "__main__":
-    dt = np.dtype([('PassengerId', np.int), ('Pclass', np.int), ('Name', np.str_, 16), ('Sex', np.str_, 16),
-    ('Age', np.float), ('SibSp', np.int), ('Parch', np.int), ('Ticket', np.int),
-    ('Fare', np.float), ('Cabin', np.str_, 16), ('Embarked', np.str_, 16)])
-
-    train_data = pd.read_csv('data/train.csv').fillna(0).values
-    test_data = pd.read_csv('data/test.csv').values
-    # print(data)
-
-    # read information into 1D arrays
-    pId = np.copy(train_data[:,0].astype(np.int))
-    y = np.copy(train_data[:,1].astype(np.int))
-    pClass = np.copy(train_data[:,2].astype(np.int))
-    name = np.copy(train_data[:,3].astype(np.str_))
-    sex = np.copy(train_data[:,4].astype(np.str_))
-    sex = np.where(sex == 'male', 0, 1)
-    age = np.copy(train_data[:,5].astype(np.float))
-    sibSp = np.copy(train_data[:,6].astype(np.int))
-    parch = np.copy(train_data[:,7].astype(np.int))
-    ticket = np.copy(train_data[:,8].astype(np.str_))
-    fare = np.copy(train_data[:,9].astype(np.int))
-    cabin = np.copy(train_data[:,10].astype(np.str_))
-    embarked = np.copy(train_data[:,11].astype(np.str_)) #TODO: map to int
-    Z = np.vstack((pClass, age))
-    # plotData2D(Z)
-    # plotClusters(Z, 2, y)
-    newY, error = linearClassifier(Z, y)
-    print(newY)
-    print(error)
+    return np.dot(X.T, W), np.sum(np.power(np.dot(X.T, W) - y, 2))/np.size(X, 1)
